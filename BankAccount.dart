6ebd51f4bed9998cfdcc92a3762e5f3ebbe2022b
mazbaul20 <mazbaul20@gmail.com>
2026-09-07 16:21:53 +0600

    Added displayAccountInfo()

--- BankAccount.dart
+++ BankAccount.dart
@@ -1,7 +1,14 @@
-class BankAccount{
-    String? accountHolderName;
-    int? accountNumber;
-    double? _balance;
+class BankAccount {
+  String? accountHolderName;
+  int? accountNumber;
+  double? _balance=0.0;
 
-    BankAccount(this.accountHolderName, this.accountNumber, this._balance);
+  BankAccount(this.accountHolderName, this.accountNumber, this._balance);
+
+  displayAccountInfo() {
+    print(
+      "Account Holder: ${this.accountHolderName}, Account Number: ${this.accountNumber}, Balance: ${this._balance?.toStringAsFixed(0)} Tk",
+    );
+  }// End method
+
 }
--- main.dart
+++ main.dart
@@ -2,6 +2,6 @@ import 'BankAccount.dart';
 
 void main(){
     print("======== Bank Account Management System ========");
-    BankAccount account = BankAccount("Rahim", 1001, 5000);
-    print(account);
+    BankAccount account = BankAccount("Rahim", 1001, 5000.49);
+    account.displayAccountInfo();
 }
