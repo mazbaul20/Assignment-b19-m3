865c70b412453cb05f91d52ffd0dc90200c3886a
mazbaul20 <mazbaul20@gmail.com>
2026-09-07 17:01:32 +0600

    Expected Output

--- main.dart
+++ main.dart
@@ -3,9 +3,13 @@ import 'BankAccount.dart';
 void main(){
     print("======== Bank Account Management System ========");
     BankAccount account1 = BankAccount("Rahim", 1001, 5000.49);
+    BankAccount account2 = BankAccount("Karim", 1002, 8000);
+
     account1.displayAccountInfo();
-    account1.deposit(1000);
+    account1.deposit(2000);
     print("After deposit: Balance: ${account1.balance.toStringAsFixed(0)} Tk");
-    account1.withdraw(500);
+    account1.withdraw(1500);
     print("After withdrawal: Balance: ${account1.balance.toStringAsFixed(0)} Tk");
+
+    account2.displayAccountInfo();
 }
