91e44c3ed994d344053f94a4a500c0ca3bcb433d
mazbaul20 <mazbaul20@gmail.com>
2026-09-07 16:48:38 +0600

    Added deposit() method

--- BankAccount.dart
+++ BankAccount.dart
@@ -1,14 +1,21 @@
 class BankAccount {
   String? accountHolderName;
   int? accountNumber;
-  double? _balance=0.0;
+  double _balance = 0.0;
 
   BankAccount(this.accountHolderName, this.accountNumber, this._balance);
 
-  displayAccountInfo() {
+  void displayAccountInfo() {
     print(
-      "Account Holder: ${this.accountHolderName}, Account Number: ${this.accountNumber}, Balance: ${this._balance?.toStringAsFixed(0)} Tk",
+      "Account Holder: ${this.accountHolderName}, Account Number: ${this.accountNumber}, Balance: ${this._balance.toStringAsFixed(0)} Tk",
     );
-  }// End method
+  } // End method
 
+  void deposit(double amount) {
+    if (amount > 0) {
+      this._balance +=amount;
+    } else {
+      print("Invalid deposit amount! Amount must be greater than 0.");
+    }
+  } // End method
 }
--- main.dart
+++ main.dart
@@ -2,6 +2,7 @@ import 'BankAccount.dart';
 
 void main(){
     print("======== Bank Account Management System ========");
-    BankAccount account = BankAccount("Rahim", 1001, 5000.49);
-    account.displayAccountInfo();
+    BankAccount account1 = BankAccount("Rahim", 1001, 5000.49);
+    account1.displayAccountInfo();
+    account1.deposit(1000);
 }
